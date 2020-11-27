6d432f98eb98bc6fe97fd886e56555dca748e5ef
rakkas95 <rakkas95@users.noreply.github.com>
2020-11-27 08:51:14 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,20 +7,20 @@ import {
  class Home extends Component {
      render() {
         console.log(this.props)
-        return (
-            <div>
-              <Jumbotron>
-                    <h1>{this.props.title}</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>  
-            </div>
-        )
+         return (
+             <div>
+                 <Jumbotron>
+                     <h1>{this.props.title}</h1>
+                     <p>
+                         This is a simple hero unit, a simple jumbotron-style component for calling
+                         extra attention to featured content or information.
+                    </p>
+                     <p>
+                         <Button variant="primary">Learn more</Button>
+                     </p>
+                 </Jumbotron>
+             </div>
+         );
     }
 }
 export default Home
